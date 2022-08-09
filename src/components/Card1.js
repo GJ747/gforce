4e3c6c9aca70560de0698b4ebdfd6f4a3e126a35
Gaurav Joshi <gauravjoshi747@gmail.com>
2022-08-09 20:53:37 +0530

    text

--- src/components/Card1.js
+++ src/components/Card1.js
@@ -12,7 +12,7 @@ function Card1() {
         <p style={{color:"#574b66",textAlign:"center", fontSize:"50px"}}>We help you to take your business online and give you a leap as compared to the competition.</p>
     </div>
         <div className='container' id='container2' >
-        <h1 style={{color:"#574b66",textAlign:"center", marginTop:"2%",fontSize:"40px"}}>Take you business online and grow ten times more</h1>
+        <h1 style={{color:"#574b66",textAlign:"center", marginTop:"2%",fontSize:"40px"}}>Take your business online and grow ten times more</h1>
         <p style={{color:"#574b66",textAlign:"center", fontSize:"20px"}}>4.9 billion people, which is 69% of the world population, use the internet. It is the world's largest platform for displaying your services and products to the public. If you do not utilise it properly, you will definitely lose future customers.</p>
         <p style={{color:"#574b66",textAlign:"center", fontSize:"30px"}}>We help you to take your business online and give you a leap as compared to the competition.</p>
     </div>
